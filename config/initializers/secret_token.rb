5527b544f2eb2796bdee7c04576f6148ecab7335
Jackie Lee <freedomprogramer@gmail.com>
2013-07-22 10:36:55 +0800

    init project with ruby1.9.3 and rails3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aozhen::Application.config.secret_token = '1506872b64c8658b0427ed8aef2514ba5445128f219dc11cae201a8d832b218d748a7c6845cde04ea356c1a6181aad73bd1768604f6377733f1b07611da6fb77'
