15583e1e2e0332400ce05fc709f1e34ed7c1cce2
Byronlee <lbyronlee@gmail.com>
2013-07-22 06:26:59 +0000

    添加测试，代码重构

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,49 +1,55 @@
+# -*- coding: utf-8 -*-
 require 'builder'
 require 'xmlsimple'
 class RobotsController < ApplicationController
 
   def index
-    @config = XmlSimple.xml_in('/home/simlegate/aozhen/app/controllers/foo.xml')    
+  #  @config = XmlSimple.xml_in(Settings.xml_path)   
+    @config = File.open(Settings.xml_path).readlines()
   end
 
   def create
-    xml = Builder::XmlMarkup.new( :indent => 2 )
-    xml.instruct! :xml, :encoding => "UTF-8"
-    xml = xml.defalut_config do |d|
-      d.length params[:lenght]
-      d.widht  params[:widht]
-      d.x      params[:x]
-      d.y      params[:y]
-      d.d      params[:d]
-    end
-    open('/home/simlegate/aozhen/app/controllers/foo.xml','w') do |f|  
-      f.puts xml  
-    end  
-
-    redirect_to :action => 'index'
-  end
-
-
-  def command
-    
-    xml = Builder::XmlMarkup.new( :indent => 2 )
-    xml.instruct! :xml, :encoding => "UTF-8"
-    xml = xml.command do |d|
-      d.value params[:optionsRadios]
-    end
-
-    EventMachine.run {
-      http = EventMachine::HttpRequest.new('http://google.com/').post :body => xml
-      http.errback { p 'Uh oh'; EM.stop }
-      http.callback {
-        p http.response_header.status
-        p http.response_header
-        p http.response
-        EventMachine.stop
-        p "________"
-      }
-    }
+    write_file(create_xml("defalut",params))
     redirect_to :action => 'index'
   end
   
+
+  def command        
+    send_http_request(create_xml("command",params[:optionsRadios]))
+    redirect_to :action => 'index'
+  end
+
+
+  private
+  def write_file xml_data
+    open(Settings.xml_path,'w') do |f|  
+      f.puts xml_data  
+    end  
+  end
+
+  def send_http_request command_xml
+    EventMachine.run {
+      http = EventMachine::HttpRequest.new(Settings.http_request_path).post :body => command_xml
+      http.errback { flash[:error]="响应请求失败！，命令没执行，从从新发送！" ; EM.stop }
+      http.callback {
+        flash[:success] ="响应请求成功，命令成功执行！"
+        write_file http.response
+        EventMachine.stop
+      }
+    }
+  end
+
+
+  def create_xml tyle,data
+    xml = Builder::XmlMarkup.new( :indent => 2 )
+    xml.instruct! :xml, :encoding => "UTF-8"    
+    return xml.command {|d| d.value data} if tyle.eql? "command"
+    xml.defalut_config do |d|
+      d.length data[:lenght]
+      d.widht  data[:widht]
+      d.x      data[:x]
+      d.y      data[:y]
+      d.d      data[:d]
+    end
+  end
 end
---
+++ spec/controllers/robots_controller_spec.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+require 'spec_helper'
+#require 'builder'
+#require 'xmlsimple'
+
+describe RobotsController do
+
+  before :each do
+#   @config_xml = XmlSimple.xml_in(Settings.xml_path)   
+  end
+
+  it "xml数据必须符合规定要求" do
+ 
+  end
+  
+  it "当接受到R指令后应该向左转" do
+  end
+
+end
